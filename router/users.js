8e9aa98762b89bcb50194e03ce1d2881f7b7dcbf
Inmuhar Algozy <inmuharalgozy@gmail.com>
2026-04-19 21:58:21 +0700

    fix usercontroller

--- router/users.js
+++ router/users.js
@@ -1,5 +1,5 @@
  const express = require('express')
-const UserController = require('../controllers/UserController')
+const UserController = require('../controllers/userController')
  const router = express.Router()
  const authentication  = require('../middlewares/authentication')
  
